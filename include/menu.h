fa72822e9708861c0615793e07bf9767023f85ee
BaronBalt <walmgren@kth.se>
2024-04-11 14:42:33 +0200

    Buttons now function properly
    1. Added the intended functionality to all buttons
    2. Added an Exit Options button with proper functionality

--- include/menu.h
+++ include/menu.h
@@ -1,14 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
 
-/// @brief Struct that represents menu buttons
-typedef struct MenuItem 
-{
-    char* text; // The text that the button displays
-    int x_start, x_end; // Start and stop x-coordinates
-    int y_start, y_end; // Start and stop y-coordinates
-    int selected; // If the button is selected or not
-} MenuItem;
+/// @brief Function for rendering all images displayed
+/// @param renderer The renderer to draw the images to
+/// @param file The file to be displayed
+/// @param x x coordinate of where the file should be displayed
+/// @param y y coordinate of where the file should be displayed
+/// @param width width of the image
+/// @param height height of the image
+/// @return returns true if there is an error loading the image
+bool renderMenuItem(SDL_Renderer* renderer, const char* file, int x, int y, int width, int height);
+
+bool options(SDL_Renderer* renderer);
 
 /// @brief Main code for the menu
 /// @param renderer The renderer to draw the menu to
--- source/menu.c
+++ source/menu.c
@@ -9,10 +9,12 @@
 #define START_GAME_IMG_PATH "resources/StartGame.png"
 #define OPTIONS_IMG_PATH "resources/Options.png"
 #define QUIT_GAME_IMG_PATH "resources/QuitGame.png"
+#define EXIT_OPTIONS_IMG_PATH "resources/ExitOptions.png"
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
 #define BUTTON_WIDTH 349
 #define BUTTON_HEIGHT 162
+#define BUTTONS_X 425
 
 bool renderMenuItem(SDL_Renderer* renderer, const char* file, int x, int y, int width, int height)
 {
@@ -31,37 +33,89 @@ bool renderMenuItem(SDL_Renderer* renderer, const char* file, int x, int y, int 
     return false;
 }
 
-bool open_menu(SDL_Renderer* renderer)
+bool options(SDL_Renderer* renderer)
 {
-    int w, h; // Texture width and height
-    bool menu = true;
+    bool options = true;
     bool closeWindow = false;
 
-    // clear the screen
     SDL_RenderClear(renderer);
 
-    // Render background and menu items
     if(renderMenuItem(renderer, BACKGROUND_IMG_PATH, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)) return closeWindow;
-    if(renderMenuItem(renderer, START_GAME_IMG_PATH, 425, 100, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
-    if(renderMenuItem(renderer, OPTIONS_IMG_PATH, 425, 300, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
-    if(renderMenuItem(renderer, QUIT_GAME_IMG_PATH, 425, 500, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
+    if(renderMenuItem(renderer, EXIT_OPTIONS_IMG_PATH, BUTTONS_X, 500, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
 
-	// main menu loop
-	while (1) {
-		// event handling
+    // Options loop
+    while (options) {
+		// Event handling
 		SDL_Event event;
         while(SDL_PollEvent(&event))
         {
             switch(event.type)
             {
             case SDL_QUIT:
-                return true; // Closes the application
+                closeWindow = true; // Closes the application
+                options = false;
                 break;
             case SDL_KEYDOWN:
                 switch(event.key.keysym.scancode)
                 {
                 case SDL_SCANCODE_ESCAPE:
-                    return false; // Returns to the game
+                    options = false; // Returns to the menu
+                    break;
+                }
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY); // Get mouse location
+                if (mouseX >= BUTTONS_X && mouseX <= BUTTONS_X + BUTTON_WIDTH && mouseY >= 500 && mouseY <= 500 + BUTTON_HEIGHT)
+                {
+                    options = false;
+                }
+                break;
+            }
+        }
+	}
+
+    return closeWindow;
+}
+
+bool open_menu(SDL_Renderer* renderer)
+{
+    int w, h; // Texture width and height
+    bool menu = true;
+    bool closeWindow = false;
+    bool menuRendered = false;
+
+	// Main menu loop
+	while (menu) {
+        if (!menuRendered)
+        {
+            // Clear the screen
+            SDL_RenderClear(renderer);
+
+            // Render background and menu items
+            if(renderMenuItem(renderer, BACKGROUND_IMG_PATH, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)) return closeWindow;
+            if(renderMenuItem(renderer, START_GAME_IMG_PATH, BUTTONS_X, 100, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
+            if(renderMenuItem(renderer, OPTIONS_IMG_PATH, BUTTONS_X, 300, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
+            if(renderMenuItem(renderer, QUIT_GAME_IMG_PATH, BUTTONS_X, 500, BUTTON_WIDTH, BUTTON_HEIGHT)) return closeWindow;
+
+            menuRendered = true;
+        }
+
+		// Event handling
+		SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+            case SDL_QUIT:
+                closeWindow = true; // Closes the application
+                menu = false;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.scancode)
+                {
+                case SDL_SCANCODE_ESCAPE:
+                    menu = false; // Returns to the game
                     break;
                 }
                 break;
@@ -69,19 +123,27 @@ bool open_menu(SDL_Renderer* renderer)
                 int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY); // Get mouse location
 
-                if (mouseX >= 425 && mouseX <= 425 + BUTTON_WIDTH && mouseY >= 100 && mouseY <= 100 + BUTTON_HEIGHT)
+                if (mouseX >= BUTTONS_X && mouseX <= BUTTONS_X + BUTTON_WIDTH && mouseY >= 100 && mouseY <= 100 + BUTTON_HEIGHT)
                 {
-                    return false; // Returns to the game
+                    menu = false;
                 }
-                if (mouseX >= 425 && mouseX <= 425 + BUTTON_WIDTH && mouseY >= 300 && mouseY <= 300 + BUTTON_HEIGHT)
+                else if (mouseX >= BUTTONS_X && mouseX <= BUTTONS_X + BUTTON_WIDTH && mouseY >= 300 && mouseY <= 300 + BUTTON_HEIGHT)
                 {
-                    return false; // Returns to the game for now...
+                    if(options(renderer) == true)
+                    {
+                        closeWindow = true;
+                        menu = false;
+                    }
+                    menuRendered = false;
                 }
-                if (mouseX >= 425 && mouseX <= 425 + BUTTON_WIDTH && mouseY >= 500 && mouseY <= 500 + BUTTON_HEIGHT)
+                else if (mouseX >= BUTTONS_X && mouseX <= BUTTONS_X + BUTTON_WIDTH && mouseY >= 500 && mouseY <= 500 + BUTTON_HEIGHT)
                 {
-                    return true; // Closes the application
+                    closeWindow = true;
+                    menu = false;
                 }
+                break;
             }
         }
 	}
+    return closeWindow;
 }
