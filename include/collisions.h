26f36fffb41e072a2fd50b59dd0a3d0971d6f1b0
juliazubko <102211232+juliazubko@users.noreply.github.com>
2024-04-12 08:30:50 +0200

    fixed @comments

--- include/collisions.h
+++ include/collisions.h
@@ -1,25 +1,15 @@
-/**
- * @brief Checks for collision between player object and the world (window boundaries and solid tiles).  
- * @param nextPosition Pointer to an SDL_Rect representing the entity's next position.  
- * @param tilemap Pointer to a TileMap containing the layout of the game world's tiles.
- *                Is used to access tile properties and determine if any solid tiles are in collision.
- * @param windowWidth The width of the game window, used to check for collisions with the right boundary.
- * @param windowHeight The height of the game window, used to check for collisions with the bottom boundary.
- * 
- * @return Returns true if a collision is detected (with either the window boundaries or solid tiles),
- *         false otherwise. 
- */
-
 #ifndef COLLISIONS_H
 #define COLLISIONS_H
 
 #include <SDL2/SDL.h>
-#include "tilemap.h"  
+#include "tilemap.h"
 
-bool collides(SDL_Rect *nextPosition, TileMap *tilemap, int windowWidth, int windowHeight); 
+/// @brief Checks for collision between player and world boundaries or solid tiles.
+/// @param nextPosition Pointer to an SDL_Rect for next position.
+/// @param tilemap Pointer to a TileMap for tile properties.
+/// @param windowWidth Width of the game window.
+/// @param windowHeight Height of the game window.
+/// @return Returns true if collision is detected, false otherwise.
+bool collides(SDL_Rect *nextPosition, TileMap *tilemap, int windowWidth, int windowHeight);
 
-
-#endif // COLLISIONS_h  
-
-
-
+#endif // COLLISIONS_H
