cd87415a8b4bccff6199d192ae2e500ded3fb236
BaronBalt <walmgren@kth.se>
2024-04-19 14:49:57 +0200

    Header update
    Added the createMenuButton function to the header file

--- include/menu.h
+++ include/menu.h
@@ -16,6 +16,15 @@ typedef struct MenuItem {
 /// @return returns true if there is an error loading the image
 bool renderMenuItem(MenuItem item);
 
+/// @brief Function for creating a menu button
+/// @param renderer The renderer to draw the button to
+/// @param text The text to display on the button
+/// @param r The red value of the button
+/// @param g The green value of the button
+/// @param b The blue value of the button
+/// @param y The y position of the button
+void createMenuButton(SDL_Renderer* renderer, char* text, int r, int g, int b, int y);
+
 bool optionsMenu(SDL_Renderer* renderer);
 
 /// @brief Main code for the menu
