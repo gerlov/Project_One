b1f235ce13e2e8d7df649a05fcf9f2357d359e7e
juliazubko <102211232+juliazubko@users.noreply.github.com>
2024-04-27 20:43:35 +0200

    change init

--- source/mazeview.c
+++ source/mazeview.c
@@ -1,4 +1,4 @@
-#include "mazeview.h"   
+#include "mazeview.h"
 
 void init_maze_view(MazeView* mazeView, SDL_Renderer* renderer, TileMap* tilemap, int window_width, int window_height) {
     // calc scale down factor based on window size 
@@ -10,18 +10,20 @@ void init_maze_view(MazeView* mazeView, SDL_Renderer* renderer, TileMap* tilemap
     int new_texture_width = tilemap->width * scaled_tile_size;
     int new_texture_height = tilemap->height * scaled_tile_size;
 
-    // a texture to render the scaled tilemap onto
+    //  texture to render the scaled tilemap 
     SDL_Texture* scaledTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, new_texture_width, new_texture_height);
     if (!scaledTexture) {
         SDL_Log("Failed to create texture: %s", SDL_GetError());
         return;
     }
 
+    // set tetxure as the target for rendering
     SDL_SetRenderTarget(renderer, scaledTexture);
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
 
+    // render each tile the tilemap on the texture
     SDL_Rect position;
     for (int y = 0; y < tilemap->height; y++) {
         for (int x = 0; x < tilemap->width; x++) {
@@ -34,37 +36,38 @@ void init_maze_view(MazeView* mazeView, SDL_Renderer* renderer, TileMap* tilemap
             position.w = scaled_tile_size;
             position.h = scaled_tile_size;
 
-            
             SDL_Texture* src_texture = (tile->type == TILE_FLOOR) ? tilemap->pFloorTexture : tilemap->pTexture;
             SDL_SetTextureBlendMode(src_texture, SDL_BLENDMODE_BLEND);
             SDL_RenderCopyEx(renderer, src_texture, &tile->src_rect, &position, 0, NULL, SDL_FLIP_NONE);
         }
     }
 
+   
     SDL_SetRenderTarget(renderer, NULL);
     SDL_SetTextureBlendMode(scaledTexture, SDL_BLENDMODE_BLEND);
 
+    // load background texture 
+    mazeView->bgTexture = IMG_LoadTexture(renderer, "resources/testpack/bkgr.png");
+    if (!mazeView->bgTexture) {
+        SDL_Log("Failed to load background texture: %s", SDL_GetError());
+        SDL_DestroyTexture(scaledTexture);
+        return;
+    }
 
-    // assign the new texture and setup view
     mazeView->texture = scaledTexture;
     mazeView->viewRect = (SDL_Rect){0, 0, new_texture_width, new_texture_height};
     mazeView->visible = 0;
-    mazeView->displayTime = 0; 
+    mazeView->displayTime = 0;
 }
-
 
 void render_maze_view(MazeView* mazeView, SDL_Renderer* renderer) {
     if (mazeView->visible && SDL_GetTicks() < mazeView->displayTime) {
-        SDL_Texture* bgTexture;
-        create_texture(&bgTexture, renderer, "resources/testpack/bkgr.png");
-        if (bgTexture) {
-            SDL_RenderCopy(renderer, bgTexture, NULL, NULL);
-            SDL_DestroyTexture(bgTexture);
+        if (mazeView->bgTexture) {
+            SDL_RenderCopy(renderer, mazeView->bgTexture, NULL, NULL);
         }
 
         int renderer_width, renderer_height;
         SDL_GetRendererOutputSize(renderer, &renderer_width, &renderer_height);
-
         SDL_Rect drawRect = {
             (renderer_width - mazeView->viewRect.w) / 2,
             (renderer_height - mazeView->viewRect.h) / 2,
@@ -76,11 +79,15 @@ void render_maze_view(MazeView* mazeView, SDL_Renderer* renderer) {
     } else {
         mazeView->visible = 0;
     }
-}  
+}
 
 void free_maze_view(MazeView* mazeView) {
     if (mazeView->texture != NULL) {
         SDL_DestroyTexture(mazeView->texture);
         mazeView->texture = NULL;
+    }
+    if (mazeView->bgTexture != NULL) {
+        SDL_DestroyTexture(mazeView->bgTexture);
+        mazeView->bgTexture = NULL;
     }
 }
