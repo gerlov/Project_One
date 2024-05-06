24e7f8f2e6eb3d325c8ba9278367de1c55458229
Majwt <theodor@familjenwase.se>
2024-05-06 15:01:34 +0200

    remove comment & add limited vision back

--- Client/src/main.c
+++ Client/src/main.c
@@ -329,7 +329,6 @@ void playing(Game_c *game)
 
     updateFromServer(game);
 
-    /// TODO: Implement kill command functionallity with server
     if (game->myCharacter->isHunter && game->space)
     {
         kill_command(game->myCharacter, game->characters, game->PLAYERS);
@@ -356,7 +355,7 @@ void playing(Game_c *game)
             draw_character(game->pRenderer, game->characters[i], i == game->activePlayer, &game->tilemap.camera);
     }
     draw_character(game->pRenderer, game->myCharacter, true, &game->tilemap.camera);
-    // drawLimitedVision(&game->lv, get_character_center(game->myCharacter), game->tilemap.camera);
+    drawLimitedVision(&game->lv, get_character_center(game->myCharacter), game->tilemap.camera);
 
     SDL_RenderPresent(game->pRenderer);
 }
