529449b260f7293476024f156b2aa511b33c30bf
juliazubko <102211232+juliazubko@users.noreply.github.com>
2024-05-09 13:20:47 +0200

    added field to char.data struct, removed diagnostic prints

--- lib/inc/data.h
+++ lib/inc/data.h
@@ -32,6 +32,10 @@ typedef struct
 } ReadyData;
 
 typedef struct {
+    int pwrID; 
+} PowerupCollected;
+
+typedef struct {
     int playerID;
     bool disconnect;
     SDL_FPoint position;
@@ -47,6 +51,7 @@ typedef struct {
     Uint32 invisiblePowerupTime;
     Uint32 frameLastUpdated;
     GameState gameState;
+    int lastPowerupCollected; 
 } CharacterData;
 
 
@@ -54,25 +59,5 @@ typedef struct {
     CharacterData characters[MAX_PLAYERS];
     bool isKilled[MAX_PLAYERS]; // 1 if player at index is killed
 } ServerData;
-
-// void printJoinData(JoinData data)
-// {
-//     printf("Seed: %d\n", data.seed);
-//     printf("Player index: %d\n", data.playerINDEX);
-//     printf("Players: %d\n", data.PLAYERS);
-//     printf("Game state: %d\n", data.gameState);
-//     printf("Ready players: ");
-//     for (int i = 0; i < data.PLAYERS; i++)
-//     {
-//         printf("%d ", data.readyPlayers[i]);
-//     }
-//     printf("\n");
-// }
-// void printServerData(ServerData data)
-// {
-    
-//     printf("Game state: %d\n", data.characters[0]);
-//     printf("\n");
-// }
 
 #endif // DATA_H
